5e1817587471a7af47f425380561e57636372c13
Rohiththe02 <101910417+Rohiththe02@users.noreply.github.com>
2022-03-20 18:53:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Rohiththe02/adv-filter-bot
+  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /EvaMaria
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
